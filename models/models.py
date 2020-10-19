4ac5559305730c1e988d1c91098e916e12b2deb8
tizycki <tmizycki@gmail.com>
2020-10-19 18:19:06 +0200

    Reduce model complexity

--- models/models.py
+++ models/models.py
@@ -17,13 +17,10 @@ class ProfileClassifier(nn.Module):
         self.input_size = input_size
         self.output_size = output_size
         
-        self.fc1 = nn.Linear(input_size, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 128)
-        self.fc4 = nn.Linear(128, 64)
-        self.fc5 = nn.Linear(64, 32)
-        self.fc6 = nn.Linear(32, 16)
-        self.fc7 = nn.Linear(16, output_size)
+        self.fc1 = nn.Linear(input_size, 64)
+        self.fc2 = nn.Linear(64, 32)
+        self.fc3 = nn.Linear(32, 16)
+        self.fc4 = nn.Linear(16, output_size)
         self.relu = nn.ReLU()
         self.dropout1 = nn.Dropout(0.3)
         
@@ -46,17 +43,5 @@ class ProfileClassifier(nn.Module):
         output = self.dropout1(output)
         
         output = self.fc4(output)
-        output = self.relu(output)
-        output = self.dropout1(output)
-        
-        output = self.fc5(output)
-        output = self.relu(output)
-        output = self.dropout1(output)
-        
-        output = self.fc6(output)
-        output = self.relu(output)
-        output = self.dropout1(output)
-        
-        output = self.fc7(output)
 
         return output
